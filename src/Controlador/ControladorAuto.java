f3fd35204d5520ac55ab29d1593ff53dabf6ce4d
Carmen <Carmen@172.16.222.1>
2019-04-24 22:10:43 -0500

    Creacion de dos controladores con los metodos del CRUD(create,read,update,delete)

--- src/Controlador/ControladorAuto.java
+++ src/Controlador/ControladorAuto.java
@@ -11,6 +11,76 @@ package Controlador;
  * @author Carmen Bravo
  *
  */
+import Modelo.Auto;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * @version 1.2
+ * @since 2019
+ * @author Carmen Bravo
+ *
+ */
 public class ControladorAuto {
+    // ATRIBUTOS DE LA CLASE
+    private SortedSet<Auto> listaOrd;  /*SORTEDSET es como tipo list que hace que la clase AUTO se contengan todos los objetos*/
+    private int codigo;
+   
+    
+    /*CRUD == CREATE, READ, UPDATE, DELETE*/
+    
+    public ControladorAuto() {
+        listaOrd = new TreeSet<>();
+        codigo = 0;
+    }
+    
+    public void create(Auto objeto){
+        codigo++;       // Empieza e cualquier valor
+        objeto.setCodigo(codigo);       
+        listaOrd.add(objeto);
+    }
+    
+    public Auto read(int codigo){
+        for (Auto auto : listaOrd) {
+            if(auto.getCodigo() == codigo){
+                return auto;
+            }
+        }
+        return null;
+    }            
+    
+    public void update(Auto objeto){
+        for(Auto auto:listaOrd){
+        if(auto.equals(objeto)){
+            listaOrd.remove(auto);
+            listaOrd.add(objeto);
+            break;
+        }
+    }
+    }
+
+    
+    public void delete(int codigo){
+        for (Auto au : listaOrd) {
+            if(au.getCodigo() == codigo){
+                listaOrd.remove(au);                
+                //listaOrdenada.clear();
+                System.out.println("Borrado....");  
+                break;
+            }            
+        }
+    }
+    
+    public void imprimirListaOrdenada(){
+        System.out.println("Lista Ordenada por nombre");
+        for (Auto auto : listaOrd) {
+            System.out.println(auto);
+        }
+    }
+    
+ 
+
+    
+    
     
 }
--- src/Controlador/ControladorFuncionario.java
+++ src/Controlador/ControladorFuncionario.java
@@ -5,6 +5,11 @@
  */
 package Controlador;
 
+import Modelo.Funcionario;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * @version 1.2
  * @since 2019
@@ -12,5 +17,81 @@ package Controlador;
  *
  */
 public class ControladorFuncionario {
+
+    /**
+     * Atributo lista
+     */
+    private List<Funcionario> list;
+    private int codigo;
     
+
+    /**
+     * constructor
+     */
+    public ControladorFuncionario() {
+        list = new ArrayList<>();
+        codigo=0;
+    }
+
+    /**
+     * que le paso un objeto funcionario y le agrego a la lista
+     *
+     * @param objeto
+     */
+    public void create(Funcionario objeto) {
+       codigo++;
+       objeto.setCodigo(codigo);
+       list.add(objeto);
+    }
+
+    /**
+     * para lerr busca la lista y retorna si lamisma es igual
+     *
+     * @param codigo
+     * @return
+     */
+    public Funcionario read(int codigo) {
+        for (Funcionario funcionario : list) {
+            if (funcionario.getCodigo() == codigo) {
+                return funcionario;
+            }
+
+        }
+        //objeto que tendra todos sus valore nulos
+        return null;
+    }
+
+    //el codigo no se actualiza solo se actualiza los nombre, apellidos,etc porque el codigo es lo primordiar porque sino depues no lo
+    //vamos a encontrar
+    public void update(Funcionario objeto) {
+        for (int i = 0; i < 10; i++) {
+            Funcionario funcionario = list.get(i);
+
+            if (funcionario.getCodigo()==objeto.getCodigo()) {
+               
+                list.set(i, objeto);
+                break;
+            }
+
+        }
+    }
+
+    public void delete(int codigo) {
+        for (int i = 0; i < 10; i++) {
+            Funcionario funcionario = list.get(i);
+
+            if (funcionario.getCodigo() == codigo) {
+                list.remove(i);
+                break;
+
+            }
+        }
+    }
+    public void listar(){
+        for (Funcionario funcionario:list){
+            System.out.println(funcionario);
+        }
+    }
+            
 }
+
