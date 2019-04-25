11cd8546ab9f1cb31ea79175092fb49783d8c556
Carmen <Carmen@172.16.222.1>
2019-04-24 22:18:31 -0500

    Creacion del controladorCliente con los metodos del CRUD(create,read,update,delete)

--- src/Controlador/ControladorCliente.java
+++ src/Controlador/ControladorCliente.java
@@ -5,12 +5,73 @@
  */
 package Controlador;
 
+import Modelo.Cliente;
+import java.util.HashSet;
+import java.util.Set;
+
+
 /**
  * @version 1.2
  * @since 2019
  * @author Carmen Bravo
  *
  */
+//clase controlador cliente
 public class ControladorCliente {
+
+    //aributos de la clase
+    private Set<Cliente> lista;
+    private int codigo; 
+
+    //constructor
+    public ControladorCliente() {
+        lista = new HashSet<>();
+        codigo = 0;
+    }
+    //metodo de crear 
+    
+    public void create(Cliente objeto){
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);        
+    }
+   /*
+     //metodo de leer
+    */
+    public Cliente read(int codigo){
+        for (Cliente cliente : lista) {
+            if(cliente.getCodigo() == codigo){
+                return cliente;
+            }
+        }        
+        return null;
+    }
+    // metodo de actualizar
+    public void update(Cliente objeto){
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+    /*
+    // metodo de eliminar
+    */
+    public void delete(int codigo){
+        for (Cliente cliente : lista) {
+            if(cliente.getCodigo() == codigo){
+                lista.remove(cliente);
+                break;
+            }
+        }  
+    } 
+    
+    /*
+    metodo de listar 
+    */
+    public void listar(){
+        for(Cliente cliente: lista){
+            System.out.println(cliente);
+        }
+    }
     
 }
