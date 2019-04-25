8ef89e32401d23113c9a95cea836595d9672cce0
Carmen <Carmen@172.16.222.1>
2019-04-24 22:45:16 -0500

    Instancias de los Controladores en la clase Principal

--- src/Vista/Principal.java
+++ src/Vista/Principal.java
@@ -5,10 +5,38 @@
  */
 package Vista;
 
+import Controlador.ControladorAuto;
+import Controlador.ControladorCamion;
+import Controlador.ControladorCliente;
+import Controlador.ControladorFuncionario;
+import java.util.Scanner;
+
 /**
- *
+ * @version 1.2
+ * @since 2019
  * @author Carmen Bravo
+ *
  */
 public class Principal {
-    
+
+  public static void main(String[] args) {
+
+
+        Scanner Sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        // creacion de las variables
+        int opcionMenuPrincipal = 0;
+        int opcionMenuFuncionario = 0;
+        int opcionMenuCliente = 0;
+        int opcionMenuAuto = 0;
+        int opcionMenuCamion = 0;
+
+        /**
+         * Instancias de los controladores
+         */
+        ControladorFuncionario controladorF = new ControladorFuncionario();
+        ControladorCliente controladorC = new ControladorCliente();
+        ControladorAuto controladorA = new ControladorAuto();
+        ControladorCamion controladorCa = new ControladorCamion();    
+}
 }
